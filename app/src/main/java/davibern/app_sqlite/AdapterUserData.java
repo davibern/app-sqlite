fd62961a223674901c13bf749b97a8286a339fa8
davibern <david.bern.pal@gmail.com>
2021-12-11 22:48:35 +0100

    Add RecyclerView with users data

--- app/src/main/java/davibern/app_sqlite/AdapterUserData.java
+++ app/src/main/java/davibern/app_sqlite/AdapterUserData.java
@@ -1,2 +1,54 @@
-package davibern.app_sqlite;public class AdapterUserData {
+package davibern.app_sqlite;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import davibern.app_sqlite.entities.User;
+
+public class AdapterUserData extends RecyclerView.Adapter<AdapterUserData.ViewHolderData> {
+
+    ArrayList<User> listUser;
+
+    public AdapterUserData(ArrayList<User> listUser) {
+        this.listUser = listUser;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderData onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_list, null, false);
+        return new ViewHolderData(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderData holder, int position) {
+        holder.txtId.setText(listUser.get(position).getId().toString());
+        holder.txtName.setText(listUser.get(position).getName());
+        holder.txtPhone.setText(listUser.get(position).getPhone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listUser.size();
+    }
+
+    public class ViewHolderData extends RecyclerView.ViewHolder {
+
+        TextView txtId, txtName, txtPhone;
+
+        public ViewHolderData(@NonNull View itemView) {
+            super(itemView);
+            txtId = itemView.findViewById(R.id.txtItemRecyclerId);
+            txtName = itemView.findViewById(R.id.txtItemRecyclerName);
+            txtPhone = itemView.findViewById(R.id.txtItemRecyclerPhone);
+        }
+
+    }
 }
--- app/src/main/java/davibern/app_sqlite/QueryUserRecyclerViewActivity.java
+++ app/src/main/java/davibern/app_sqlite/QueryUserRecyclerViewActivity.java
@@ -1,14 +1,53 @@
 package davibern.app_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import davibern.app_sqlite.entities.User;
+import davibern.app_sqlite.utilities.Utilities;
+
 public class QueryUserRecyclerViewActivity extends AppCompatActivity {
+
+    ArrayList<User> listUser;
+    RecyclerView recyclerView;
+    ConectionSQLiteHelper conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_user_recycler_view);
+
+        conn = new ConectionSQLiteHelper(getApplicationContext(), "bd_users", null, 1);
+        listUser = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerId);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        queryListUsers();
+
+        AdapterUserData adapterUserData = new AdapterUserData(listUser);
+        recyclerView.setAdapter(adapterUserData);
+    }
+
+    private void queryListUsers() {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        User user = null;
+        Cursor cursor = db.rawQuery("select * from " + Utilities.TABLE_USER, null);
+
+        while(cursor.moveToNext()) {
+            user = new User();
+            user.setId(cursor.getInt(0));
+            user.setName(cursor.getString(1));
+            user.setPhone(cursor.getString(2));
+
+            listUser.add(user);
+        }
     }
 }
